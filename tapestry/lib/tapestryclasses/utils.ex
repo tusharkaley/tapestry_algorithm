36b7f83059f5ef431c4ee7273ec5fa1bbe0d2d7c
tusharkaley <tomcat1903>
2019-10-19 20:17:17 -0400

    Truncated GUID to 8 digits. Also checked for uniqueness

--- tapestry/lib/tapestryclasses/utils.ex
+++ tapestry/lib/tapestryclasses/utils.ex
@@ -3,9 +3,29 @@ defmodule Tapestryclasses.Utils do
 		Function to get the child Spec for the workers
 	"""
 	def add_children(child_class, num_nodes, script_pid) do
-		Enum.each 1..num_nodes, fn(x) ->
-        {:ok, _child} = Supervisor.start_child(Tapestryclasses.Supervisor, %{:id => x, :start => {child_class, :start_link, [x]}, :restart => :transient,:type => :worker})
-	  end
-		Supervisor.start_child(Tapestryclasses.Supervisor, %{:id => :tracker, :start => {Tapestryclasses.Aggregator, :start_link, [script_pid]}, :restart => :transient,:type => :worker})
+
+    map = Enum.reduce 1..num_nodes, %{}, fn x, acc ->
+
+      # Generating SHA1 for the index
+      sha_id = :crypto.hash(:sha, to_string(x)) |> Base.encode16
+      # Slicing out the lest significant 8 digits and checking for collisions
+      sha_id = if Map.has_key?(acc, String.slice(sha_id, -8, 8)) do
+          # Moving over the slicing window by 1 so as to avoid collisions
+          String.slice(sha_id, -9, 8)
+        else
+          # No collisions FTW
+          String.slice(sha_id, -8, 8)
+        end
+      {:ok, child} = Supervisor.start_child(Tapestryclasses.Supervisor, %{:id => sha_id, :start => {child_class, :start_link, [x]}, :restart => :transient,:type => :worker})
+
+      Map.put(acc, sha_id, child)
+    end
+    IO.inspect(map)
+    # Enum.each 1..num_nodes, fn(x) ->
+
+
+	  # end
+		Supervisor.start_child(Tapestryclasses.Supervisor, %{:id => :aggregator, :start => {Tapestryclasses.Aggregator, :start_link, [script_pid]}, :restart => :transient,:type => :worker})
 	end
+
 end
